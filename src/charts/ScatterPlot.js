00fa0fa64b329c79395589a1a3af62484ac63c78
Spokauskaite <53835940+Spokauskaite@users.noreply.github.com>
2021-02-21 12:44:29 -0500

    Modifying scatterplot

--- src/charts/ScatterPlot.js
+++ src/charts/ScatterPlot.js
@@ -4,46 +4,112 @@ import * as d3 from 'd3'
 const ScatterPlot = ({data}) => {
   const scatterPlot = useRef(null)
   const drawScatterPlot = (data) => {
-    // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 30, bottom: 30, left: 30},
-    width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    // set parameters
+    var margin = {top: 30, right: 30, bottom: 50, left: 50}
+    const width = 600 - margin.left - margin.right
+    const height = 400 - margin.top - margin.bottom
+    const maxX = d3.max( data.map( d => d.bill_length_mm ))
+    const maxY = d3.max( data.map( d => d.bill_depth_mm ))
+    const labelMargin = 30
 
+    // Create svg
     const svg = d3.select(scatterPlot.current)
-    .append('svg')
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style('border', '1px solid black')
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")")  
+      .append('svg')
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .style('border', '1px solid #ddd')
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")")  
 
     // Add X axis
     const x = d3.scaleLinear()
-    .domain([0, d3.max(data.map(val=>val.bill_length_mm))])
-    .range([ 0, width ])
+      .domain([0, maxX]).nice()
+      .range([ 0, width ])
     svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-
+      .attr("transform", "translate(0," + height + ")")
+      .attr("class", "xAxis")
+      .call(d3.axisBottom(x).tickSize(0))
+      .call(g => g.select(".domain").remove())
+      .append("text")
+      .attr("y", labelMargin)
+      .attr("x", x(maxX)-labelMargin)
+      .attr("fill", "#000000")
+      .text("Bill Length (mm)")
+    
     // Add Y axis
     const y = d3.scaleLinear()
-    .domain([0, d3.max(data.map(val=>val.bill_depth_mm))])
-    .range([ height, 0]);
+      .domain([0, maxY]).nice()
+      .range([ height, 0])
     svg.append("g")
-    .call(d3.axisLeft(y))
+      .attr("class", "yAxis")
+      .call(d3.axisLeft(y).tickSize(0))
+      .call(g => g.select(".domain").remove())
+      .append("text")
+      .attr("y", -labelMargin)
+      .attr("x", y(maxY)-labelMargin)
+      .attr("transform", "rotate(-90)")
+      .attr("fill", "#000000")
+      .text("Bill Depth (mm)")
+
+    // Add colors
+    const color = d3.scaleOrdinal()
+      .domain(data.map(d => d.species))
+      .range(d3.schemeCategory10)
+    // Add shape
+    const shape = d3.scaleOrdinal()
+      .domain(data.map(d => d.species))
+      .range(d3.symbols.map(s => d3.symbol().type(s)()))
     
-    const dataPoints = Object.entries(data)
     // Add dots
+    const dataPoints = Object.entries(data)
     svg.append('g')
-    .selectAll("dot")
-    .data(dataPoints)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) { return x(d[1].bill_length_mm) } )
-    .attr("cy", function (d) { return y(d[1].bill_depth_mm) } )
+      .selectAll("dot")
+      .data(dataPoints)
+      .enter()
+      .append("circle")
+      .attr("cx", d => x(d[1].bill_length_mm))
+      .attr("cy", d => y(d[1].bill_depth_mm))
       .attr("r", 3)
-      .style("fill", "#69b3a2")
+      .style("fill", d => color(d[1].species)) 
+
+    svg.selectAll("g.yAxis g.tick")
+      .append("line")
+      .attr("class", "gridline")
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", width)
+      .attr("y2", 0)
+  
+    svg.selectAll("g.xAxis g.tick")
+      .append("line")
+      .attr("class", "gridline")
+      .attr("x1", 0)
+      .attr("y1", -height)
+      .attr("x2", 0)
+      .attr("y2", 0)
+
+    // Add legend
+    const legend = svg.selectAll(".legend")
+    .data(color.domain())
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => "translate(" + i * 100  + ",0)" )
+
+    // Add legend circles
+    legend.append("circle")
+      .attr("cx", 5)
+      .attr("cy", -15)
+      .attr("r", 5)
+      .style("fill", color)
+
+    // Add legend text
+    legend.append("text")
+      .attr("x", 20)
+      .attr("y", -15)
+      .attr("dy", ".35em")
+      .style("text-anchor", "start")
+      .text( d => d)
   }
 
   useEffect(()=>{
